beb2598d372c25dcfecab88a904c9d1958bda557
Vince <hvhn3295@gmail.com>
2024-11-25 17:42:12 +0700

    data structures and algorithms in java 6th ed chapter 3 25.11.2024

---
+++ ds&a6thedexercises/src/chapter3/using_array/Scoreboard.java
@@ -0,0 +1,12 @@
+package chapter3.using_array;
+
+public class Scoreboard {
+
+	private int numEntries = 0;
+	private GameEntry[] board;
+	
+	public Scoreboard(int capacity) {
+		board = new GameEntry[capacity];
+	}
+	
+}
