4f0933745bdaafc589593961004d20e576c17483
Huynh Vu Hoang Nguyen <hvhn3295@gmail.com>
2024-11-26 00:12:44 +0700

    ...

--- ds&a6thedexercises/src/chapter3/using_array/Scoreboard.java
+++ ds&a6thedexercises/src/chapter3/using_array/Scoreboard.java
@@ -4,9 +4,25 @@ public class Scoreboard {
 
 	private int numEntries = 0;
 	private GameEntry[] board;
-	
+
 	public Scoreboard(int capacity) {
 		board = new GameEntry[capacity];
 	}
-	
+
+	public void add(GameEntry entry) {
+		int newScore = entry.getScore();
+
+		if (numEntries < board.length || newScore > board[numEntries - 1].getScore()) {
+			if (numEntries < board.length) {
+				numEntries++;
+			}
+			int j = numEntries - 1;
+			while (j > 0 && board[j - 1].getScore() < newScore) {
+				board[j] = board[j - 1];
+				j--;
+			}
+			board[j] = entry;
+		}
+	}
+
 }
