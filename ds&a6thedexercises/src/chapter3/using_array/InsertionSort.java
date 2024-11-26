eae7e57d86cab7d3152cd7d683a973e85eae5706
Vince <hvhn3295@gmail.com>
2024-11-26 17:15:37 +0700

    data structures and algorythms in java 6ed 26.11.2024

---
+++ ds&a6thedexercises/src/chapter3/using_array/InsertionSort.java
@@ -0,0 +1,22 @@
+package chapter3.using_array;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void insertionSort(char[] data) {
+		int n = data.length;
+		for(int i = 1; i < n; i++) {
+			char cur = data[i];
+			int j = i;
+			while(j > 0 && data[j-1] > cur) {
+				data[j] = data[j-1];
+				j--;
+			}
+			data[j] = cur;
+		}
+	}
+	
+}
